617ed1172324f9afe182be2965fe32d0b7091c40
Esteban Pastor <estebanandrespastor@gmail.com>
2023-12-18 05:20:27 -0300

    Add controller and POST method for cancidates
    Now the candidates can send a form request to the front-end and also supports pdf files

---
+++ backend/Controllers/CandidateController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using backend.Core.Context;
+using backend.Core.Dtos.Candidate;
+using backend.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private ApplicationDbContext _context { get; }
+        private IMapper _mapper { get; }
+
+        public CandidateController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateCandidate([FromForm] CandidateCreateDto dto, IFormFile pdfFile)
+        {
+            var fiveMegaBytes = 5 * 1024 * 1024;
+            var pdfMimeType = "application/pdf";
+
+            if (pdfFile.Length > fiveMegaBytes || pdfFile.ContentType != pdfMimeType)
+            {
+                return BadRequest("File is not valid");
+            }
+
+            var resumeUrl = Guid.NewGuid().ToString() + ".pdf";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Documents", "pdfs", resumeUrl);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await pdfFile.CopyToAsync(stream);
+            }
+            var newCandidate = _mapper.Map<Candidate>(dto);
+            newCandidate.ResumeUrl = resumeUrl;
+            await _context.Candidates.AddAsync(newCandidate);
+            await _context.SaveChangesAsync();
+
+            return Ok("Candidate saved successfully");
+        }
+
+    }
+}
--- backend/Core/AutoMapperConfig/AutoMapperConfigProfile.cs
+++ backend/Core/AutoMapperConfig/AutoMapperConfigProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using backend.Core.Dtos.Candidate;
 using backend.Core.Dtos.Company;
 using backend.Core.Dtos.Job;
 using backend.Core.Entities;
@@ -15,7 +16,9 @@ namespace backend.Core.AutoMapperConfig
             CreateMap<Job, JobGetDto>()
                 .ForMember(dest => dest.CompanyName, opt => opt.MapFrom(src => src.Company.Name));
 
-
+            CreateMap<CandidateCreateDto, Candidate>();
+            CreateMap<Candidate, CandidateGetDto>()
+                .ForMember(dest => dest.JobTitle, opt => opt.MapFrom(src => src.Job.Title));
 
         }
     }
