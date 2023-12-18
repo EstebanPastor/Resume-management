3e58a022819dd95d37625739b6ef9b9209efa444
Esteban Pastor <estebanandrespastor@gmail.com>
2023-12-18 02:07:54 -0300

    Add connection to the db and migrated the entitites with code first

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,12 @@
+using backend.Core.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("local"));
+});
 
 builder.Services.AddControllers();
 
