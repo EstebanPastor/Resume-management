382f6d7ac6f2231c74236baa5dbe7a162f634f20
Esteban Pastor <estebanandrespastor@gmail.com>
2023-12-18 09:49:08 -0300

    Add custom spinner and home page section with routes, also added lazy loading to the page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
-import { useContext } from "react";
+import { useContext, lazy, Suspense } from "react";
 import { ThemeContext } from "./context/theme.context"; 
+import {Routes, Route} from "react-router-dom";
 import NavBar from "./components/ui/navbar/NavBar";
+import CustomLinearProgress from "./components/ui/linear-progress/CustomLinearProgress";
+
+const Home = lazy(() => import("./pages/home/Home"));
 
 function App() {
   const {darkMode}  = useContext(ThemeContext);
@@ -10,7 +14,13 @@ function App() {
   return (<div className={appStyles}>
     <NavBar />
     <div className="wrapper">
+      <Suspense fallback={<CustomLinearProgress />}>
 
+    
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </Suspense>
     </div>
   </div>)
 }
---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className="content home">
+      <h3>Welcome</h3>
+      <br />
+      <br />
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate fuga pariatur placeat voluptatibus, distinctio aliquam sunt voluptates accusamus autem sit tempora, ducimus ad nam? Necessitatibus cupiditate nulla commodi maiores sit?</span>
+    </div>
+  )
+}
+
+export default Home
